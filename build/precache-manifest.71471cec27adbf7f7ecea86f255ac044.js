b9d9a4e8b18a2cf44e34c3ee822bf51cf28e1e21
Sigi Jaeckel <sigij55@gmail.com>
2020-06-26 12:54:41 -0400

    run build

---
+++ build/precache-manifest.71471cec27adbf7f7ecea86f255ac044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8f7dfc92cf4dae74007802e659fe17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d54e78f8b6d36f9fbb43",
+    "url": "/static/css/main.893ef5d8.chunk.css"
+  },
+  {
+    "revision": "baeb1cc2234893fc3b32",
+    "url": "/static/js/2.d5ff3856.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d5ff3856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54e78f8b6d36f9fbb43",
+    "url": "/static/js/main.46283d1e.chunk.js"
+  },
+  {
+    "revision": "6aabe9abeec7e6620162",
+    "url": "/static/js/runtime-main.6d29312b.js"
+  },
+  {
+    "revision": "6f3d4789a3e7a58781fcbd0cc17d495b",
+    "url": "/static/media/image.6f3d4789.jpg"
+  }
+]);
